2806a3fc138f2b06a44ef56438a112aa8fe11a4c
Peter Renström <renstrom.peter@gmail.com>
2014-11-25 09:27:36 +0100

    Update for Warlords of Draenor (and some refactoring).

--- nFrameHider.lua
+++ nFrameHider.lua
@@ -1,14 +1,13 @@
-
--- Un-comment a frame in order to active frame hiding
+-- Un-comment a frame to activate frame hiding
 local frameList = {
 	'oUF_Neav_Player',
-	--'oUF_Neav_Pet',
-	--'oUF_Neav_Target',
-	--'oUF_Neav_TargetTarget',
-	--'oUF_Neav_Focus',
-	--'oUF_Neav_FocusTarget,
-	--'oUF_Neav_Party',
-	--'oUF_Neav_Raid',
+	'oUF_Neav_Pet',
+	'oUF_Neav_Target',
+	'oUF_Neav_TargetTarget',
+	'oUF_Neav_Focus',
+	'oUF_Neav_FocusTarget',
+	'oUF_Neav_Party',
+	'oUF_Neav_Raid',
 }
 
 -- Alpha value for when out of combat
@@ -16,10 +15,11 @@ local outOfCombatAlpha = 0
 
 -------------------------------------------------------------------------------
 
+local f = CreateFrame('Frame')
 local EnabledFrames = {}
 
-local function CheckHealthAndMana()
-	return UnitHealth('player') == UnitHealthMax('player') and UnitPower('player', SPELL_POWER_MANA) >= UnitPowerMax('player', SPELL_POWER_MANA) * 0.95
+local function FullHealth()
+	return UnitHealth('player') == UnitHealthMax('player')
 end
 
 local function FadeIn(frame)
@@ -28,68 +28,70 @@ local function FadeIn(frame)
 end
 
 local function FadeOut(frame)
-	UIFrameFadeOut(frame, 0.35, frame:GetAlpha(), outOfCombatAlpha)
-	frame:EnableMouse(false)
+	if not InCombatLockdown() then
+		UIFrameFadeOut(frame, 0.35, frame:GetAlpha(), outOfCombatAlpha)
+		frame:EnableMouse(false)
+	end
 end
 
-local function OnEvent(self, event, unit)
-	if (event == 'PLAYER_REGEN_DISABLED') then
-		for _, frame in ipairs(EnabledFrames) do
+-- Need to wait for the PLAYER_ENTERING_WORLD event before
+-- accessing the oUF elements
+function addon:PLAYER_ENTERING_WORLD()
+	f:UnregisterEvent('PLAYER_ENTERING_WORLD')
+
+	for _, frame in ipairs(frameList) do
+		if frame == 'oUF_Neav_Raid' then
+			-- Loop through all the raid groups
+			for i = 1, oUF_Neav.units.raid.numGroups do
+				table.insert(EnabledFrames, _G[frame..i])
+				_G[frame..i]:SetAlpha(outOfCombatAlpha)
+			end
+		else
+			table.insert(EnabledFrames, _G[frame])
+			_G[frame]:SetAlpha(outOfCombatAlpha)
+		end
+	end
+end
+
+function addon:UNIT_HEALTH()
+	for _, frame in pairs(EnabledFrames) do
+		if FullHealth() then
+			FadeOut(frame)
+		else
 			FadeIn(frame)
 		end
-
-		self:UnregisterEvent('UNIT_HEALTH')
-		self:UnregisterEvent('UNIT_POWER')
-	elseif (event == 'PLAYER_REGEN_ENABLED') then
-		if (CheckHealthAndMana()) then
-			for _, frame in ipairs(EnabledFrames) do
-				FadeOut(frame)
-			end
-		end
-
-		self:RegisterEvent('UNIT_HEALTH')
-		self:RegisterEvent('UNIT_POWER')
 	end
+end
 
-	-- Only runs when out of combat and health/mana < 100%
-	if (unit == 'player' and event == 'UNIT_HEALTH' or event == 'UNIT_POWER') then
-		for _, frame in ipairs(EnabledFrames) do
-			if (CheckHealthAndMana()) then
-				FadeOut(frame)
-			else
-				FadeIn(frame)
-			end
-		end
+function addon:PLAYER_REGEN_DISABLED()
+	for _, frame in pairs(EnabledFrames) do
+		FadeIn(frame)
 	end
+end
 
-	-- Need to wait for the PLAYER_ENTERING_WORLD event before
-	-- accessing the oUF elements
-	if (event == 'PLAYER_ENTERING_WORLD') then
-		self:UnregisterEvent('PLAYER_ENTERING_WORLD')
-
-		for _, frame in ipairs(frameList) do
-			if (frame ~= 'oUF_Neav_Raid') then
-				table.insert(EnabledFrames, _G[frame])
-				_G[frame]:SetAlpha(outOfCombatAlpha)
-			else
-				-- Loop through all the raid groups
-				for i = 1, oUF_Neav.units.raid.numGroups do
-					table.insert(EnableFrames, _G[frame..i])
-					_G[frame..i]:SetAlpha(outOfCombatAlpha)
-				end
-			end
+function addon:PLAYER_REGEN_ENABLED()
+	if FullHealth() then
+		for _, frame in pairs(EnabledFrames) do
+			FadeOut(frame)
 		end
 	end
 end
 
-local f = CreateFrame('Frame')
--- Only register events if we're actually going to iterate any elements
-if (#frameList > 0) then
-	f:RegisterEvent('PLAYER_REGEN_DISABLED')
-	f:RegisterEvent('PLAYER_REGEN_ENABLED')
-	f:RegisterEvent('UNIT_HEALTH')
-	f:RegisterEvent('UNIT_POWER')
-	f:RegisterEvent('PLAYER_ENTERING_WORLD')
+f:RegisterEvent('PLAYER_REGEN_DISABLED')
+f:RegisterEvent('PLAYER_REGEN_ENABLED')
+f:RegisterEvent('PLAYER_ENTERING_WORLD')
+f:RegisterUnitEvent('UNIT_HEALTH', 'player')
 
-	f:SetScript('OnEvent', OnEvent)
+f:SetScript('OnEvent', function(event, ...)
+	addon[event](...)
+end)
+
+for _, frame in pairs(frameList) do
+	_G[frame]:HookScript('OnEnter', function()
+		FadeIn(frame)
+	end)
+
+	_G[frame]:HookScript('OnLeave', function()
+		FadeOut(frame)
+	end)
 end
